7b8d889500a4f595fd177b5fdd2a67ef81508ebc
FelipeTorquato <felipectorquato1@gmail.com>
2025-02-23 01:48:18 -0300

    fixing swagger and front end requests

--- back-end/src/main/java/com/controle/controle_gastos/ControleDeGastosResidencialApplication.java
+++ back-end/src/main/java/com/controle/controle_gastos/ControleDeGastosResidencialApplication.java
@@ -2,9 +2,28 @@ package com.controle.controle_gastos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Configuration
 public class ControleDeGastosResidencialApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE",
+								"CONNECT");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleDeGastosResidencialApplication.class, args);
